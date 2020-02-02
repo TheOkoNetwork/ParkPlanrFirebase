61bc11abd5375f328860faf7051eb0ce2925220b
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-02-02 02:41:26 +0000

    CircleCI update, Config.js

--- public/js/Config.js
+++ public/js/Config.js
@@ -3,3 +3,6 @@ config.firebaseDevEnviromentProject="parkplanr-dev";
 config.SiteDefaultTitle="ParkPlanr";
 
 config.DevEnviromentFirebaseFunctionsUrl="https://us-central1-parkplanr-dev.cloudfunctions.net";
+
+
+config.version="{{APP_VERSION_HERE}}"
