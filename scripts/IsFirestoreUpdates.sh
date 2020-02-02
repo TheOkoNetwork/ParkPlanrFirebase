a8f21a73e10378f0115f97ea3c6de051bfef82ba
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-02-02 02:25:41 +0000

    CI script fixes

--- scripts/IsFirestoreUpdates.sh
+++ scripts/IsFirestoreUpdates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 existingJSONIndexes="$(cat firestore.indexes.json)"
-indexes="$(firebase firestore:indexes)"
+indexes="$(firebase firestore:indexes --token ${FIREBASE_TOOLS_TOKEN})"
 exitCode=$?
 if [ $exitCode -ne 0 ];then
 	echo "Failed to fetch indexes from firestore"
@@ -10,6 +10,6 @@ fi
 if [ "$existingJSONIndexes" == "$indexes" ];then
 	echo "local version of firestore indexes is up to date"
 else
-	echo "local version of firestore indexes is out of date, run UpdateFirestore.sh"
+	echo "local version of firestore indexes is out of date, run ./scripts/UpdateFirestore.sh"
 	exit 1
 fi
