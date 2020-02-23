0bcbb80a6dd33d3185b65ca7255e144d3ff0011a
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-02-23 13:59:42 +0000

    Pulled in adminlte template

---
+++ hosting/admin/js/config.js
@@ -0,0 +1,8 @@
+config={};
+config.firebaseDevEnviromentProject="parkplanr-dev";
+config.SiteDefaultTitle="ParkPlanr";
+
+config.DevEnviromentFirebaseFunctionsUrl="https://us-central1-parkplanr-dev.cloudfunctions.net";
+
+
+config.version="{{APP_VERSION_HERE}}"
