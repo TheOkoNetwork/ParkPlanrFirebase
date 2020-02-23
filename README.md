dd33e817cdf47a8ffbc7cdfb9957c652a5f3f9f4
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-02-23 01:43:19 +0000

    Moved single public site folder into sub folder of hosting, created admin site folder (no content), enabled firebase hosting multi site (hosting/site=dev.parkplanr.app,hosting/admin=admin.dev.parkplanr.app)

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Conditional CSS classes
 
 Authenticated=.show() if authenticated, .hide() if not
 Unauthenticated=.show() if NOT authenticated, .hide() if authenticated
+
+
+
+firebase target:apply hosting site parkplanr-dev
+firebase target:apply hosting admin parkplanradmindev
---
+++ hosting/admin/js/Config.js
@@ -0,0 +1,8 @@
+config={};
+config.firebaseDevEnviromentProject="parkplanr-dev";
+config.SiteDefaultTitle="ParkPlanr Administration website";
+
+config.DevEnviromentFirebaseFunctionsUrl="https://us-central1-parkplanr-dev.cloudfunctions.net";
+
+
+config.version="ADM_{{APP_VERSION_HERE}}"
