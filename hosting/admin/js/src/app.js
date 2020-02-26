64fc6baa82aa9a3b86e874772873d5894d70c29c
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2020-02-26 15:11:07 +0000

    Added sign out

--- hosting/admin/js/src/app.js
+++ hosting/admin/js/src/app.js
@@ -20,27 +20,48 @@ const init = async () => {
   // initialize firebase
   firebase.initializeApp(firebaseConfig)
 
+
+  $('#signoutButton').on('click', function () {
+    signout()
+  })
+
+
+
   // when a user signs in, out or is first seen this session in either state
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       $('.currentUsername').text(firebase.auth().currentUser.displayName)
       $('.userProfileImage').prop('src', firebase.auth().currentUser.photoURL)
+      userAuthenticated(user);
     } else {
       console.log('User is unauthenticated')
       window.location = '/signin'
     }
   })
-
-  //  firebase.auth().currentUser.getIdTokenResult().then((idTokenResult) => {
-  //     if (!parkplanr.appfirebase.auth().currentUser.getIdTokenResult() .then((idTokenResult) => { // Confirm the user is an Admin. if (!!idTokenResult.claims.admin) { console.log("I am an admin"); } else { console.log("I am not an admin, i should not be here"); window.location.href="https://parkplanr.app" } }) .catch((error) => { console.log(error); });f!idTokenResult.claims.admin) {
-  //       console.log("I am an admin");
-  //     } else {
-  //       console.log("I am not an admin, i should not be here");
-  //        window.location.href="https://parkplanr.app"
-  //     }
-  //  }).catch((error) => {
-  //    console.log(error);
-  //  });
 }
 
+
+function userAuthenticated(user) {
+  console.log(user);
+
+  firebase.auth().currentUser.getIdTokenResult().then((idTokenResult) => {
+    // Confirm the user is an Admin.
+    if (idTokenResult.claims.admin) {
+      console.log('I am an admin')
+    } else {
+      console.log('I am not an admin, i should not be here')
+      window.location.href = 'https:parkplanr.app'
+    }
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+
+function signout() {
+  firebase.auth().signOut().then(function() {
+    console.log("Signed out");
+  }).catch(function(error) {
+    console.log("Error signing out",error);
+  });
+}
 init()
