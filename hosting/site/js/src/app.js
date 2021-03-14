ccc42358b6e292fd7445c8ebe65b292fb6a86756
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-03-14 15:32:42 +0000

    Prettiered site app.js

--- hosting/site/js/src/app.js
+++ hosting/site/js/src/app.js
@@ -1,261 +1,260 @@
-import { config } from './config.js'
-import { stateUrl } from './stateUrl.js'
-import { ridecountHomeLoad, ridecountImportLoad } from './ridecount.js'
+import { config } from "./config.js";
+import { stateUrl } from "./stateUrl.js";
+import { ridecountHomeLoad, ridecountImportLoad } from "./ridecount.js";
 
-const firebase = require('firebase/app').default
-window.firebase = firebase
-const Navigo = require('navigo')
-require('firebase/auth')
-require('firebase/firestore')
-require('firebase/storage')
-const $ = window.$
-window.stateData = {}
+const firebase = require("firebase/app").default;
+window.firebase = firebase;
+const Navigo = require("navigo");
+require("firebase/auth");
+require("firebase/firestore");
+require("firebase/storage");
+const $ = window.$;
+window.stateData = {};
 
 function getCurrentUser() {
   return new Promise((resolve, reject) => {
-     const unsubscribe = window.auth.onAuthStateChanged(user => {
-        unsubscribe();
-        resolve(user);
-     }, reject);
+    const unsubscribe = window.auth.onAuthStateChanged((user) => {
+      unsubscribe();
+      resolve(user);
+    }, reject);
   });
 }
 
 const init = async () => {
-  console.log(`I am running on version: ${config('version')}`)
+  console.log(`I am running on version: ${config("version")}`);
 
-  console.table(config())
+  console.table(config());
 
   // fetch firebase configuration json
-  const response = await window.fetch('/__/firebase/init.json')
-  const firebaseConfig = await response.json()
-  console.table(firebaseConfig)
+  const response = await window.fetch("/__/firebase/init.json");
+  const firebaseConfig = await response.json();
+  console.table(firebaseConfig);
 
   // initialize firebase
-  firebase.initializeApp(firebaseConfig)
-  window.db = firebase.firestore()
-  $('body').trigger('dbLoaded')
+  firebase.initializeApp(firebaseConfig);
+  window.db = firebase.firestore();
+  $("body").trigger("dbLoaded");
 
-  window.storage = firebase.storage()
-  $('body').trigger('storageLoaded')
+  window.storage = firebase.storage();
+  $("body").trigger("storageLoaded");
 
-  window.auth = firebase.auth()
-  $('body').trigger('authLoaded')
+  window.auth = firebase.auth();
+  $("body").trigger("authLoaded");
 
   // when a user signs in, out or is first seen this session in either state
   window.auth.onAuthStateChanged(function (user) {
     if (user) {
-      $('.currentUsername').text(window.auth.currentUser.displayName)
-      $('.userProfileImage').prop('src', window.auth.currentUser.photoURL)
-      userAuthenticated(user)
-      $('.showIfAuthenticated').show()
-      $('.showIfUnauthenticated').hide()
+      $(".currentUsername").text(window.auth.currentUser.displayName);
+      $(".userProfileImage").prop("src", window.auth.currentUser.photoURL);
+      userAuthenticated(user);
+      $(".showIfAuthenticated").show();
+      $(".showIfUnauthenticated").hide();
     } else {
-      console.log('User is unauthenticated')
-      $('.showIfAuthenticated').hide()
-      $('.showIfUnauthenticated').show()
+      console.log("User is unauthenticated");
+      $(".showIfAuthenticated").hide();
+      $(".showIfUnauthenticated").show();
     }
-  })
-
+  });
 
   router.on({
-    '/': function () {
-      console.log('I am on the home page')
-      loadPage('index')
+    "/": function () {
+      console.log("I am on the home page");
+      loadPage("index");
     },
-    '/signout': async function () {
-      console.log('I am on the signout page');
+    "/signout": async function () {
+      console.log("I am on the signout page");
       await getCurrentUser();
-        console.log('Auth Loaded, sign in flow')
-        var signoutSplit = window.location.hash.split('signout=')
-        if (signoutSplit.length > 1) {
-          console.log("Sign out flow complete");
-          window.auth.signOut()
-          window.router.navigate("/");
+      console.log("Auth Loaded, sign in flow");
+      var signoutSplit = window.location.hash.split("signout=");
+      if (signoutSplit.length > 1) {
+        console.log("Sign out flow complete");
+        window.auth.signOut();
+        window.router.navigate("/");
+      } else {
+        console.log("No post sign out flag, redirect to authcore");
+        var service = location.hostname;
+        let authCoreUrl;
+        console.log(redirectUrl);
+        if (location.hostname == "dev.parkplanr.app") {
+          authCoreUrl = "auth.dev.parkplanr.app";
         } else {
-          console.log("No post sign out flag, redirect to authcore");
-          var service = location.hostname;
-          let authCoreUrl;
-          console.log(redirectUrl);
-          if (location.hostname == "dev.parkplanr.app") {
-            authCoreUrl = "auth.dev.parkplanr.app"
-          } else {
-            authCoreUrl = "auth.parkplanr.app"
-          }
-          console.log(`Detected auth core URL: ${authCoreUrl}`);
-          var redirectUrl = `https://${authCoreUrl}/signout#service=${service}`;
-          console.log(`Got redirect url: ${redirectUrl}`);        
-          location.href = redirectUrl;
+          authCoreUrl = "auth.parkplanr.app";
         }
+        console.log(`Detected auth core URL: ${authCoreUrl}`);
+        var redirectUrl = `https://${authCoreUrl}/signout#service=${service}`;
+        console.log(`Got redirect url: ${redirectUrl}`);
+        location.href = redirectUrl;
+      }
     },
-    '/signin': async function () {
-      console.log('I am on the signin page');
+    "/signin": async function () {
+      console.log("I am on the signin page");
       await getCurrentUser(window.auth);
-        console.log('Auth Loaded, sign in flow')
-        var tokenSplit = window.location.hash.split('token=')
-        if (tokenSplit.length > 1) {
-            console.log("Got auth token, attempt sign in with custom token");
-            try {
-                firebase.auth().signInWithCustomToken(tokenSplit[1]);
-                const postAuthUrl = localStorage.postAuthUrl || "/";
-                delete(localStorage.postAuthUrl);
-                window.router.navigate(postAuthUrl);
-            } catch(err) {
-                console.log("Error signing in with custom token");
-                console.log(err);
-  //             window.alert("Failed signing in");
-                ///window.location = "/signin";
-            };
+      console.log("Auth Loaded, sign in flow");
+      var tokenSplit = window.location.hash.split("token=");
+      if (tokenSplit.length > 1) {
+        console.log("Got auth token, attempt sign in with custom token");
+        try {
+          firebase.auth().signInWithCustomToken(tokenSplit[1]);
+          const postAuthUrl = localStorage.postAuthUrl || "/";
+          delete localStorage.postAuthUrl;
+          window.router.navigate(postAuthUrl);
+        } catch (err) {
+          console.log("Error signing in with custom token");
+          console.log(err);
+          //             window.alert("Failed signing in");
+          ///window.location = "/signin";
+        }
+      } else {
+        console.log("No token, redirect to authcore");
+        var service = location.hostname;
+        let authCoreUrl;
+        console.log(redirectUrl);
+        if (location.hostname == "dev.parkplanr.app") {
+          authCoreUrl = "auth.dev.parkplanr.app";
         } else {
-            console.log("No token, redirect to authcore");
-            var service = location.hostname;
-            let authCoreUrl;
-            console.log(redirectUrl);
-            if (location.hostname == "dev.parkplanr.app") {
-              authCoreUrl = "auth.dev.parkplanr.app"
-            } else {
-              authCoreUrl = "auth.parkplanr.app"
-            }
-            console.log(`Detected auth core URL: ${authCoreUrl}`);
-            var redirectUrl = `https://${authCoreUrl}/signin#service=${service}`;
-            console.log(`Got redirect url: ${redirectUrl}`);        
-            location.href = redirectUrl;
-        };  
+          authCoreUrl = "auth.parkplanr.app";
+        }
+        console.log(`Detected auth core URL: ${authCoreUrl}`);
+        var redirectUrl = `https://${authCoreUrl}/signin#service=${service}`;
+        console.log(`Got redirect url: ${redirectUrl}`);
+        location.href = redirectUrl;
+      }
     },
-    '/ridecount': function () {
-      console.log('I am on the ridecount page')
-      loadPage('ridecount')
+    "/ridecount": function () {
+      console.log("I am on the ridecount page");
+      loadPage("ridecount");
     },
-    '/ridecount/import': function () {
-      console.log('I am on the ridecount import page')
-      loadPage('ridecount/import')
-    }
-  })
-  
+    "/ridecount/import": function () {
+      console.log("I am on the ridecount import page");
+      loadPage("ridecount/import");
+    },
+  });
+
   router.notFound(function () {
-    console.log('Route not found')
-    load404()
-  })
-  
+    console.log("Route not found");
+    load404();
+  });
+
   $(document).ready(function () {
-    $('.defaultFragmentHolder').each(function () {
-      const fragment = $(this).data('fragmentid')
-      window.loadFragment(fragment)
-    })
-  
-    router.resolve()
-  })
+    $(".defaultFragmentHolder").each(function () {
+      const fragment = $(this).data("fragmentid");
+      window.loadFragment(fragment);
+    });
+
+    router.resolve();
+  });
+};
+
+function userAuthenticated(user) {
+  console.log(user);
 }
 
-function userAuthenticated (user) {
-  console.log(user)
-}
-
-function load404 () {
+function load404() {
   // $('#contentDiv').html('4 oh 4')
-  loadPage('404')
+  loadPage("404");
 }
 
 window.loadFragment = function (fragment) {
-  console.log(`Loading fragment: ${fragment}`)
+  console.log(`Loading fragment: ${fragment}`);
   $.get(`/fragments/${fragment}.html`, function (data) {
     try {
-      const isFragment = data.startsWith('<!-- FRAGMENT CONTENT-TAG_ID -->')
+      const isFragment = data.startsWith("<!-- FRAGMENT CONTENT-TAG_ID -->");
 
       if (!isFragment) {
-        console.log('Fragment HTML file not found')
+        console.log("Fragment HTML file not found");
         // load404();
-        return
+        return;
       }
 
       $(`.fragmentHolder[data-fragmentid="${fragment}"]`).each(function () {
-        $(this).replaceWith(data)
-      })
-      router.updatePageLinks()
+        $(this).replaceWith(data);
+      });
+      router.updatePageLinks();
 
-      const today = new Date()
-      $('.currentYear').text(today.getFullYear())
-      $('.currentVersion').text(config('version'))
+      const today = new Date();
+      $(".currentYear").text(today.getFullYear());
+      $(".currentVersion").text(config("version"));
 
       switch (fragment) {
-        case 'header':
-          $('#signOutButton').on('click', function () {
+        case "header":
+          $("#signOutButton").on("click", function () {
             window.router.navigate("/signout");
-          })
-          break
+          });
+          break;
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
       // bugsnagClient.notify(error);
       // showFatalErrorPage(error);
     }
   }).fail(function (error) {
-    console.log('Failed loading fragment')
+    console.log("Failed loading fragment");
     // bugsnagClient.notify(error);
-    console.log(error)
-  })
-}
+    console.log(error);
+  });
+};
 
-function loadPage (page, params) {
+function loadPage(page, params) {
   $.get(`/pages/${page}.html`, function (data) {
     try {
-      const isPage = data.startsWith('<!-- PAGE CONTENT-TAG_ID -->')
+      const isPage = data.startsWith("<!-- PAGE CONTENT-TAG_ID -->");
       const isStandalonePage = data.startsWith(
-        '<!-- STANDALONE PAGE CONTENT-TAG_ID -->'
-      )
+        "<!-- STANDALONE PAGE CONTENT-TAG_ID -->"
+      );
       if (!isPage && !isStandalonePage) {
-        console.log('Page HTML file not found')
-        if (page === '404') {
-          $('#contentDiv').html('4 oh 4')
+        console.log("Page HTML file not found");
+        if (page === "404") {
+          $("#contentDiv").html("4 oh 4");
         } else {
-          loadPage('404', params)
+          loadPage("404", params);
         }
-        return
+        return;
       }
 
       // loads the page content into the dom
       if (isPage) {
-        if (!$('#contentDiv').length) {
+        if (!$("#contentDiv").length) {
           console.log(
-            'Switching from standalone page, loading standard page core layout'
-          )
-          $('#body').html(
+            "Switching from standalone page, loading standard page core layout"
+          );
+          $("#body").html(
             '<script id="header_Holder">LoadFragment("header");</script><main id="contentDiv"></main><script id="footer_Holder">LoadFragment("footer");</script>'
-          )
+          );
         }
-        $('#contentDiv').html(data)
+        $("#contentDiv").html(data);
       } else {
-        $('body').html(data)
+        $("body").html(data);
       }
-      router.updatePageLinks()
+      router.updatePageLinks();
 
       // updates any tags with the class CurrentYear with the YYYY year
-      $('.currentYear').text(new Date().getFullYear())
+      $(".currentYear").text(new Date().getFullYear());
       // scrolls back to the top of the window
-      window.scrollTo(0, 0)
+      window.scrollTo(0, 0);
 
       switch (page) {
-        case 'ridecount':
-          ridecountHomeLoad(params)
-          break
-        case 'ridecount/import':
-          ridecountImportLoad(params)
-          break
-        }
+        case "ridecount":
+          ridecountHomeLoad(params);
+          break;
+        case "ridecount/import":
+          ridecountImportLoad(params);
+          break;
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
       // showFatalErrorPage(error, 'PPERPGCA')
     }
   }).fail(function (error) {
-    console.log(error)
+    console.log(error);
     // showFatalErrorPage(error, 'PPERPGLD')
-  })
+  });
 }
 
-console.log(stateUrl())
+console.log(stateUrl());
 
-const root = `https://${window.location.href.split('/')[2]}/`
-const useHash = false
-const hash = '#!' // Defaults to: '#'
-const router = new Navigo(root, useHash, hash)
+const root = `https://${window.location.href.split("/")[2]}/`;
+const useHash = false;
+const hash = "#!"; // Defaults to: '#'
+const router = new Navigo(root, useHash, hash);
 window.router = router;
-init()
+init();
