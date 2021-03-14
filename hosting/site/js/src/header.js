3f6ff9059799690698667d31f598af0adf9edafd
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-03-14 15:56:02 +0000

    Add const let

--- hosting/site/js/src/header.js
+++ hosting/site/js/src/header.js
@@ -3,10 +3,10 @@ const Template7 = require('template7').default
 async function initHeaderParksDropdown () {
   const dropdownTemplate = $('#templateHeaderParksDropdown').html();
   const compiledDropdownTemplate = Template7.compile(dropdownTemplate);  
-  parkDocs = await window.db.collection("parks").where("active","==",true).get();
+  const parkDocs = await window.db.collection("parks").where("active","==",true).get();
   parkDocs.forEach(function(parkDoc) {
     console.log(parkDoc);
-    parkData = parkDoc.data();
+    let parkData = parkDoc.data();
     parkData.id = parkDoc.id;
     console.log(parkData);
     $('#headerParksDropdown').append(compiledDropdownTemplate(parkData));
