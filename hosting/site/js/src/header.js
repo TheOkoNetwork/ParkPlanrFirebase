649e69edc9cf0323f8764588bdfa464b6e6f0e2a
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-03-14 16:04:31 +0000

    Sort park header name

--- hosting/site/js/src/header.js
+++ hosting/site/js/src/header.js
@@ -3,7 +3,11 @@ const Template7 = require('template7').default
 async function initHeaderParksDropdown () {
   const dropdownTemplate = $('#templateHeaderParksDropdown').html();
   const compiledDropdownTemplate = Template7.compile(dropdownTemplate);  
-  const parkDocs = await window.db.collection("parks").where("active","==",true).get();
+  const parkDocs = await window.db
+    .collection("parks")
+    .where("active","==",true)
+    .orderBy("name.name","asc")
+    .get();
   parkDocs.forEach(function(parkDoc) {
     console.log(parkDoc);
     let parkData = parkDoc.data();
