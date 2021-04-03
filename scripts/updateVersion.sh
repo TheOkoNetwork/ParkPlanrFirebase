c0d0bf8700cfca83547ed130d51e706ec079d500
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-04-03 16:00:49 +0000

    no branch_ is not a var bash

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 branch=$(echo "$GITHUB_REF"|awk -F'/' '{print $3}')
-newVersion="$branch_${GITHUB_SHA::8}"
+newVersion="$branch""_${GITHUB_SHA::8}"
 echo $newVersion
 sed -i 's/{{APP_VERSION_HERE}}/'"$newVersion"'/g' hosting/POM/js/src/config.js
