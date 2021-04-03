add697bc9d5addaf1a4bb70087629b3e727d5ce2
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-04-03 15:52:31 +0000

    branch and short sha

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-newVersion="$GITHUB_REF $GITHUB_SHA"
+branch = $(echo $GITHUB_REF|awk -F'/' '{print $3}')
+newVersion="$branch_${GITHUB_SHA::8}"
 echo $newVersion
 sed -i 's/{{APP_VERSION_HERE}}/'"$newVersion"'/g' hosting/POM/js/src/config.js
