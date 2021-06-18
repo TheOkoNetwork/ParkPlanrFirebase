7790406359f60f0e0414f37a06e5438ec2c74c08
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-06-18 12:20:57 +0000

    Dev container stuff

---
+++ scripts/install-dev-tools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install jq -y
+sudo npm install -g npm
+sudo npm install -g standard prettier
+sudo npm install -g firebase-tools
+echo "Please close and reopen any open terminals"
